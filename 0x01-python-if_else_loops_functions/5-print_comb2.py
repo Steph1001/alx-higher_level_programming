f91de171b10e3aa443154ebac2644d4cdc40f754
steph1001 <stephizunwa@gmail.com>
2022-06-28 15:18:21 -0700

    /root/ACP

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:0>2d}, ".format(i), end="")
-    if i == 99:
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
         print("{:d}".format(i))
