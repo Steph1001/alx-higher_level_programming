91d53303d3815d37d57967d79706753e85886b05
steph1001 <stephizunwa@gmail.com>
2022-06-30 10:48:48 -0700

    /root/ACP

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as Steph
-a = 1
-b = 2
 if __name__ == "__main__":
+    import add_0 as Steph
+    a = 1
+    b = 2
     print("{:d} + {:d} = {:d}".format(a, b, Steph.add(a, b)))
