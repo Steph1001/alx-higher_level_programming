6fbf4cdf843e3237fa066cb4edd57c3802e3583b
steph1001 <stephizunwa@gmail.com>
2022-06-30 10:43:15 -0700

    /root/ACP

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as Steph
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, Steph.add(a, b)))
