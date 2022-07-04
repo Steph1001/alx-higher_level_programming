139025937db66825c8f8db2d88c751c8d59f5c5d
steph1001 <stephizunwa@gmail.com>
2022-07-04 07:53:44 -0700

    /root/ACP

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for n in matrix:
         for i in n:
-            print("{}".format(i), end=" " if i != n[-1] else "")
+            print("{:d}".format(i), end=" " if i != n[-1] else "")
